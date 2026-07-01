3a5ae3760870e9502f6f2789c96de94f5e4d6595
ramon chuinca <ramonchuinca@hotmail.com>
2026-07-01 17:50:42 -0400

    atualizado

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index() // <= Iisso e Um methodo que vai ser chamado quando a rota for acessada
-    {
-        return view('main'); // <= Isso é para retornar a view main.blade.php
-    }
+  
 }
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,3 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/main',[MainController::class, 'index']);
