bab6249bdf54bf88802a6213d39eb483d44f77f8
ramon chuinca <ramonchuinca@hotmail.com>
2026-07-06 17:55:32 -0400

    teste ainda em andamento

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,26 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
+        // Form validation
+        $request->validate(
+
+            // Regras
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:20|max:50',
+            ],
+
+            // Mensagens
+            [
+                'text_username.required' => 'O username é obrigatório.',
+                'text_username.email' => 'O username deve ser um e-mail válido.',
+
+                'text_password.required' => 'O password é obrigatório.',
+                'text_password.min' => 'O password deve ter pelo menos :min caracteres.',
+                'text_password.max' => 'O password deve ter no máximo :max caracteres.',
+            ]
+        );
+
         return 'login submit';
     }
 
